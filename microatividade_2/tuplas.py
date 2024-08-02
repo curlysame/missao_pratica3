7b1e6627d890b68e690cc36aaf9f6b81c1e337e8
curlysame <118316442+curlysame@users.noreply.github.com>
2024-08-01 21:32:40 -0300

    Create tuplas.py

---
+++ microatividade_2/tuplas.py
@@ -0,0 +1,13 @@
+primeira_tupla = (1,2,3,4,"Olá, tupla")
+print(primeira_tupla)
+indice = primeira_tupla.index(3)
+print(indice)
+if 3 in primeira_tupla:
+    print("A tupla contem o elemento 3.")
+else:
+    print("A tupla não contem o elemento 3.")
+
+if 33 in primeira_tupla:
+    print("A tupla contem o elemento 33.")
+else:
+    print("A tupla não contem o elemento 33.")    
